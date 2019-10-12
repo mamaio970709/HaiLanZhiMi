b0424994023a613c5d8fb5952a4d4b47d12872c9
mamiao <2623773963@qq.com>
2019-10-12 09:08:56 +0800

    第一版

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-const gulp = require("gulp")
-
+const gulp = require("gulp");
+const sass = require("gulp-sass");
 
 
 
@@ -9,5 +9,19 @@ gulp.task("watchall",async ()=>{
     gulp.watch("*.html",async()=>{
         gulp.src("*.html")
         .pipe(gulp.dest("c:\\dat35\\HaiLanZhiMi"))
+    })
+
+
+    //监听saaa文件
+    gulp.watch("sass/**/*",async ()=>{
+        gulp.src("sass/**/*")
+        .pipe(sass())
+        .pipe(gulp.dest("c:\\dat35\\HaiLanZhiMi\\css"))
+    })
+
+    //监听js，进行复制
+    gulp.watch("js/*.js",async()=>{
+        gulp.src("js/*.js")
+        .pipe(gulp.dest("c:\\dat35\\HaiLanZhiMi\\js"))
     })
 })
