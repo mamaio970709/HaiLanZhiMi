adaefd78d1d2c6f081f02c2eb765fc2319d10d93
mamiao <2623773963@qq.com>
2019-10-12 12:11:26 +0800

    2019.10.12上午

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,29 @@ const sass = require("gulp-sass");
 
 
 
-//监听任务
+// 监听任务
 gulp.task("watchall",async ()=>{
-    //监听html，进行复制
+    // 监听html，进行复制
     gulp.watch("*.html",async()=>{
         gulp.src("*.html")
-        .pipe(gulp.dest("c:\\dat35\\HaiLanZhiMi"))
+        .pipe(gulp.dest("c:\\phpstudy\\www\\HaiLanZhiMi"))
     })
 
 
-    //监听saaa文件
-    gulp.watch("sass/**/*",async ()=>{
-        gulp.src("sass/**/*")
+    //监听sass文件
+    gulp.watch("*.scss",async ()=>{
+        gulp.src("*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("c:\\dat35\\HaiLanZhiMi\\css"))
+        .pipe(gulp.dest("c:\\phpstudy\\www\\HaiLanZhiMi\\css"))
     })
 
     //监听js，进行复制
     gulp.watch("js/*.js",async()=>{
         gulp.src("js/*.js")
-        .pipe(gulp.dest("c:\\dat35\\HaiLanZhiMi\\js"))
+        .pipe(gulp.dest("c:\\phpstudy\\www\\HaiLanZhiMi\\js"))
     })
 })
+
+// gulp.task("copy",function(){
+//     return gulp.src("./**/*").pipe(gulp.dest("C:\\phpStudy\\WWW\\HaiLanZhiMi"));
+// });
